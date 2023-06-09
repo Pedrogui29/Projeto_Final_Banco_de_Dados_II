78737aa8f95569ad1035ce078491948452f6d1bf
Pedro Guilherme <pedropgfo@gmail.com>
2023-06-09 13:45:06 -0300

    Initial commit

---
+++ SimpsonsDatabase.py
@@ -0,0 +1,107 @@
+class SimpsonsDatabase:
+    def __init__(self, database):
+        self.db = database
+
+
+
+    def create_member(self, name, profissao, idade):
+        query = "CREATE (:Member {name: $name, profissao: $profissao, idade: $idade})"
+        parameters = {"name": name, "profissao": profissao, "idade": idade}
+        self.db.execute_query(query, parameters)
+
+    def create_member_from_user_input(self):
+        name = input("Digite o nome do membro: ")
+        profissao = input("Digite a profissão do membro: ")
+        idade = input("Digite a idade do membro: ")
+
+        query = "CREATE (:Member {name: $name, profissao: $profissao, idade: $idade})"
+        parameters = {"name": name, "profissao": profissao, "idade": idade}
+        self.db.execute_query(query, parameters)
+        print("Membro Simpson Criado com sucesso")
+
+    def create_animal_member(self, name, pet):
+        query = "CREATE (:Member {name: $name, pet: $pet})"
+        parameters = {"name": name, "pet": pet}
+        self.db.execute_query(query, parameters)
+
+    def get_member(self):
+        query = "MATCH (p:Member) RETURN p.name AS name, p.profissao AS profissao, p.idade AS idade"
+        results = self.db.execute_query(query)
+        return [(result["name"], result["profissao"], result["idade"]) for result in results]
+
+    def get_animal_member(self):
+        query = "MATCH (p:Member) RETURN p.name AS name, p.pet AS pet"
+        results = self.db.execute_query(query)
+        return [(result["name"], result["pet"]) for result in results]
+
+    def update_member(self):
+        old_name = input("Digite o nome do membro que deseja alterar: ")
+        new_name = input("Digite o novo nome do membro: ")
+
+        update_profissao = input("Deseja atualizar a profissão do membro? (S/N): ")
+        if update_profissao.upper() == "S":
+            new_profissao = input("Digite a nova profissão do membro: ")
+        else:
+            new_profissao = None
+
+        update_idade = input("Deseja atualizar a idade do membro? (S/N): ")
+        if update_idade.upper() == "S":
+            new_idade = input("Digite a nova idade do membro: ")
+        else:
+            new_idade = None
+
+        query = "MATCH (p:Member {name: $old_name}) "
+        if new_name:
+            query += "SET p.name = $new_name "
+        if new_profissao:
+            query += "SET p.profissao = $new_profissao "
+        if new_idade:
+            query += "SET p.idade = $new_idade "
+
+        parameters = {
+            "old_name": old_name,
+            "new_name": new_name,
+            "new_profissao": new_profissao,
+            "new_idade": new_idade
+        }
+
+        self.db.execute_query(query, parameters)
+        print("Dados do membro atualizados com sucesso")
+
+    def delete_member(self):
+        name = input("Digite o membro que deseja deletar: ")
+        query = "MATCH (p:Member {name: $name}) DETACH DELETE p"
+        parameters = {"name": name}
+        self.db.execute_query(query, parameters)
+
+    def create_parents_relationship(self, parent_name, child_name):
+        query = "MATCH (parent:Member {name: $parent_name}), (child:Member {name: $child_name}) CREATE (parent)-[:pai]->(child)"
+        parameters = {"parent_name": parent_name, "child_name": child_name}
+        self.db.execute_query(query, parameters)
+
+    def create_married_relationship(self, esposo1_name, esposo2_name):
+        query = "MATCH (esposo1:Member {name: $esposo1_name}), (esposo2:Member {name: $esposo2_name}) CREATE (esposo1)-[:casado]->(esposo2)"
+        parameters = {"esposo1_name": esposo1_name, "esposo2_name": esposo2_name}
+        self.db.execute_query(query, parameters)
+
+    def create_boyfriend_relationship(self, boyfriend_name, girlfriend_name):
+        query = "MATCH (boyfriend:Member {name: $boyfriend_name}), (girlfriend:Member {name: $girlfriend_name}) CREATE (boyfriend)-[:namorado]->(girlfriend)"
+        parameters = {"boyfriend_name": boyfriend_name, "girlfriend_name": girlfriend_name}
+        self.db.execute_query(query, parameters)
+
+    def create_pet_relationship(self, owner_name, pet_name):
+        query = "MATCH (owner:Member {name: $owner_name}), (pet:Member {name: $pet_name}) CREATE (owner)-[:dono]->(pet)"
+        parameters = {"owner_name": owner_name, "pet_name": pet_name}
+        self.db.execute_query(query, parameters)
+
+
+    def query_family_graph(self, question):
+        query = None
+        if "Estudante" in question:
+            query = "MATCH (member:Member {profissao: 'Estudante'}) RETURN member.name AS name"
+        if query:
+            result = self.db.execute_query(query)
+            return [record["name"] for record in result]
+        else:
+            return "Desculpe, não entendi a pergunta."
+
---
+++ main.py
@@ -0,0 +1,93 @@
+from Database import Database
+from SimpsonsDatabase import SimpsonsDatabase
+
+db = Database("bolt://3.239.247.32:7687", "neo4j", "reserves-margins-boresight")
+db.drop_all()
+
+members_database = SimpsonsDatabase(db)
+
+
+def cria_simpsons():
+    members_database.create_member("Clancy", "Aposentado", 95)
+    members_database.create_member("Abe", "Aposentado", 83)
+    members_database.create_member("Marge", "Dona de casa", 38)
+    members_database.create_member("Homer", "Inspetor de segurança", 38)
+    members_database.create_member("Lisa", "Estudante", 8)
+    members_database.create_member("Maggie", "sem profissao", 1)
+    members_database.create_member("Bart", "Estudante", 10)
+    members_database.create_member("Ned", "Balconista", 60)
+    members_database.create_member("Maude", "Ativista", 50)
+    members_database.create_member("Rod", "Estudante", 9)
+    members_database.create_member("Todd", "Estudante", 7)
+    members_database.create_member("Mona", "artista", 100)
+    members_database.create_animal_member("Bola de neve", "Gato")
+    members_database.create_animal_member("Ajudante do papai Noel", "Cachorro")
+    members_database.create_parents_relationship("Abe", "Homer")
+    members_database.create_parents_relationship("Clancy", "Marge")
+    members_database.create_parents_relationship("Homer", "Bart")
+    members_database.create_parents_relationship("Homer", "Lisa")
+    members_database.create_parents_relationship("Homer", "Maggie")
+    members_database.create_parents_relationship("Marge", "Bart")
+    members_database.create_parents_relationship("Marge", "Lisa")
+    members_database.create_parents_relationship("Marge", "Maggie")
+    members_database.create_parents_relationship("Mona", "Ned")
+    members_database.create_parents_relationship("Ned", "Rod")
+    members_database.create_parents_relationship("Ned", "Todd")
+    members_database.create_parents_relationship("Maude", "Rod")
+    members_database.create_parents_relationship("Maude", "Todd")
+    members_database.create_pet_relationship("Bart", "Ajudante do papai Noel")
+    members_database.create_pet_relationship("Lisa", "Bola de neve")
+    members_database.create_married_relationship("Homer", "Marge")
+    members_database.create_married_relationship("Ned", "Maude")
+
+
+def exibir_menu():
+    print("Bem-vindo(a) ao programa!")
+    print("Aqui estão as ações disponíveis:")
+    print("1. Adicionar novo Membro")
+    print("2. Descobrir quais pessoas possuem determinada profissao")
+    print("3. Fazer o update de um membro")
+    print("4. Deletar um membro")
+    print("5. Sair")
+
+
+def executar_acao(acao):
+    if acao == 1:
+        print("Executando a Ação 1...")
+        members_database.create_member_from_user_input()
+    elif acao == 2:
+        print("Executando a Ação 2...")
+        pergunta = input("Digite a profissao que deseja buscar: ")
+        resultado = members_database.query_family_graph(pergunta)
+        print(resultado)
+    elif acao == 3:
+        print("Executando a Ação 3...")
+        members_database.update_member()
+    elif acao == 4:
+        print("Executando a Ação 4")
+        members_database.delete_member()
+    elif acao == 5:
+        return False
+    else:
+        print("Opção inválida. Por favor, escolha uma ação válida.")
+
+    return True
+
+
+# Função principal
+def main():
+    cria_simpsons()
+    print("Grafo Simpsons criado")
+    exibir_menu()
+    executar = True
+    while executar:
+        acao = int(input("Escolha uma ação (1-4): "))
+        executar = executar_acao(acao)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+db.close()
